20ec8375af36282711f8bd465d5dfafdb728f300
Sylviachelimo <chelimosylvia280@gmail.com>
2024-03-25 22:13:13 +0300

    Completed

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,28 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        
+          <ProjectItem
+            key= "1" 
+            name={"Reciplease"}
+            about={"A recipe tracking app"}
+            technologies={["Rails", "Bootstrap CSS"]} 
+          />
+      
+        <ProjectItem
+          key="2"
+          name={"Kibbles N Bitz"}
+          about={"A pet care application"}
+          technologies={["React", "Node.js", "MongoDB"]}
+        />
+        <ProjectItem
+          key="3"
+          name={"Alienwares"}
+          about={"An e-commerce website for alien gadgets"}
+          technologies={["React", "Redux", "Express", "MongoDB"]}
+        />
+      </div>
     </div>
   );
 }
